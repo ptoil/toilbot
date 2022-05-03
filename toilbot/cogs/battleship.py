cbd729bce1092dfade63b87afb3f3c4b19925bec
ptoil <suhailmallick@gmail.com>
2022-05-03 10:05:27 -0400

    bs board progress

--- toilbot/cogs/battleship.py
+++ toilbot/cogs/battleship.py
@@ -10,9 +10,6 @@ from PIL import Image, ImageDraw
 
 ########## GLOBALS
 
-BOT_OWNER = 205908835435544577
-GUMIES_ID = 569942936939134988
-
 emoji_first_place  = "🥇"
 emoji_second_place = "🥈"
 emoji_third_place  = "🥉"
@@ -51,41 +48,50 @@ class Battleship(commands.Cog):
 
 	@commands.command()
 	async def drawBoard(self, ctx):
-		im = Image.new("RGBA", (1000, 1000), (0, 0, 0, 255))
+		im = Image.new("RGBA", (712, 800), (0, 0, 0, 255))
 		draw = ImageDraw.Draw(im)
 #		draw.rectangle([(25, 25), (375, 325)], fill=(0, 0, 255))
 #		for j in range(1):
 #			for i in range(15):
-		for i in range(8):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(0*(2*math.sqrt(3)/2)*30), 200+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+		"""width  of hexagon is side*2, each is offset by 3/4 the width, therefore multiply by 3/2"""
+		"""height of hexagon is side*sqrt(3)"""
+		lightblue = (111, 223, 225)
+		darkblue = ()
+		lightgreen = (185, 222, 106)
+		darkgreen = ()
+		for i in range(4):
+			draw.line(list(self.hexagon_generator(30, offset=(25+(0*(3/2)*30), 200+(i*(math.sqrt(3))*30)))), fill=lightgreen, width=4)
+		for i in range(4):
+			draw.line(list(self.hexagon_generator(30, offset=(25+(0*(3/2)*30), (200+(math.sqrt(3)*30)*4)+(i*(math.sqrt(3))*30)))), fill=lightblue, width=4)
+
 		for i in range(9):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(1*(2*math.sqrt(3)/2)*30), 175+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(1*(3/2)*30), 175+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(10):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(2*(2*math.sqrt(3)/2)*30), 150+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(2*(3/2)*30), 150+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(11):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(3*(2*math.sqrt(3)/2)*30), 125+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(3*(3/2)*30), 125+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(12):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(4*(2*math.sqrt(3)/2)*30), 100+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(4*(3/2)*30), 100+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(13):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(5*(2*math.sqrt(3)/2)*30), 75+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(5*(3/2)*30), 75+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(14):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(6*(2*math.sqrt(3)/2)*30), 50+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(6*(3/2)*30), 50+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(14):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(7*(2*math.sqrt(3)/2)*30), 25+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(7*(3/2)*30), 25+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(14):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(8*(2*math.sqrt(3)/2)*30), 50+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(8*(3/2)*30), 50+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(13):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(9*(2*math.sqrt(3)/2)*30), 75+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(9*(3/2)*30), 75+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(12):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(10*(2*math.sqrt(3)/2)*30), 100+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(10*(3/2)*30), 100+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(11):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(11*(2*math.sqrt(3)/2)*30), 125+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(11*(3/2)*30), 125+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(10):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(12*(2*math.sqrt(3)/2)*30), 150+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(12*(3/2)*30), 150+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(9):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(13*(2*math.sqrt(3)/2)*30), 175+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(13*(3/2)*30), 175+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 		for i in range(8):
-			draw.line(list(self.hexagon_generator(30, offset=(25+(14*(2*math.sqrt(3)/2)*30), 200+(i*(2*math.sqrt(3)/2)*30)))), fill=(0, 0, 255), width=10)
+			draw.line(list(self.hexagon_generator(30, offset=(25+(14*(3/2)*30), 200+(i*(math.sqrt(3))*30)))), fill=(0, 0, 255), width=4)
 
 		await self.sendImage(im, ctx)
 	
