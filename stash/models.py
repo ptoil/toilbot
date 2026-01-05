c675521aeaf586dbd9cd830797c300c419ad1d29
Suhail Mallick <suhailmallick@gmail.com>
2026-01-04 23:56:43 -0600

    cards layout

--- stash/models.py
+++ stash/models.py
@@ -10,6 +10,7 @@ class File(models.Model):
 	file = models.FileField() #move to temp folder if model is deleted (acts as recycle bin, isnt fully deleted until admin confirms)
 	description = models.TextField(blank=True)
 	source = models.CharField(blank=True)
+#	author = models.CharField(blank=True)
 	nsfw = models.BooleanField(default=False, verbose_name="NSFW")
 #	related = models.JSONField()
 
@@ -17,8 +18,8 @@ class File(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	modified = models.DateTimeField(auto_now=True)
 	added_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-	file_size_bytes = models.PositiveIntegerField()
-	file_video_length = models.DurationField()
+#	file_size_bytes = models.PositiveIntegerField()
+#	file_video_length = models.DurationField()
 
 
 	def file_type(self):
