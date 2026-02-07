34e672a305b82ed895267d1c9fb7f6817be31f19
Suhail Mallick <suhailmallick@gmail.com>
2026-02-07 01:38:36 -0600

    htmx sort direction toggle

--- stash/views.py
+++ stash/views.py
@@ -6,10 +6,50 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from stash.models import File
 
-# Create your views here.
+
 class FileListView(ListView):
 	model = File
-#	paginate_by = 10
+#	paginate_by = 2
+
+	
+	def get_template_names(self):
+		template_names = super().get_template_names()
+
+		if self.request.headers.get("HX-Request"):
+			return [f"{i}#content-partial" for i in template_names]
+		else:
+			return template_names
+
+	def get_queryset(self):
+		qs = super(FileListView, self).get_queryset()
+
+		sort = self.request.GET.get("sort", default="modified")
+		direction = self.request.GET.get("direction", default="desc")
+
+		prefix = '-' if direction == "desc" else ''
+
+		qs = qs.order_by(f"{prefix}{sort}")
+		return qs
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		sort = self.request.GET.get("sort", default="modified")
+		direction = self.request.GET.get("direction", default="desc")
+
+		context["current_sort"] = sort
+		context["current_direction"] = direction
+		context["next_direction"] = "asc" if direction == "desc" else "desc"		
+
+		print(context["current_direction"], context["next_direction"])
+
+		return context
+
+#	def get(self, request, *args, **kwargs):
+#		super().get(request)
+
+
+
 
 class FileDetailView(DetailView):
 	model = File
