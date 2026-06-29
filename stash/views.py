bc5c4728c1968c7de5784e6b7499494b3ebded07
Suhail Mallick <suhailmallick@gmail.com>
2026-06-29 14:14:44 -0500

    pagination oob resolves to last page

--- stash/views.py
+++ stash/views.py
@@ -4,7 +4,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
+from django.core.paginator import InvalidPage
 
 from stash.models import File, Profile
 
@@ -43,6 +44,34 @@ class FileListView(ListView):
 
 		qs = qs.order_by(f"{prefix}{sort}")
 		return qs
+
+	#Overriding to handle page oob error from switching filters. changing behavior to load last page
+	def paginate_queryset(self, queryset, page_size):
+		paginator = self.get_paginator(
+			queryset,
+			page_size,
+			orphans=self.get_paginate_orphans(),
+			allow_empty_first_page=self.get_allow_empty(),
+		)
+
+		page_kwarg = self.page_kwarg
+		page = self.kwargs.get(page_kwarg) or self.request.GET.get(page_kwarg) or 1
+		
+		try:
+			page_number = int(page)
+		except ValueError:
+			if page == "last":
+				page_number = paginator.num_pages
+			else:
+				raise Http404(
+					_("Page is not “last”, nor can it be converted to an int.")
+				)
+		try:
+			page = paginator.page(page_number)
+		except InvalidPage as e:
+			page = paginator.page(paginator.num_pages)
+
+		return (paginator, page, page.object_list, page.has_other_pages())
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
