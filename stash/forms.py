38cf9fc7148468e35e69331383044be063bdb726
Suhail Mallick <suhailmallick@gmail.com>
2026-08-03 13:41:54 -0500

    collections field allows creation

--- stash/forms.py
+++ stash/forms.py
@@ -1,11 +1,26 @@
 from django import forms
 from easy_select2.widgets import Select2Multiple
-from .models import File
+from .models import File, Collection
+
+class CollectionsModelMultipleChoiceField(forms.ModelMultipleChoiceField):
+	def clean(self, value):
+		values = value or []
+		PKs = []
+		for raw in values:
+			if raw.isdigit() and self.queryset.filter(pk=raw).exists():
+				PKs.append(raw)
+			else:
+				new_collection, _ = Collection.objects.get_or_create(name=raw)
+				PKs.append(new_collection.pk)
+		return super().clean(PKs)
+
 
 class FileForm(forms.ModelForm):
+	collections = CollectionsModelMultipleChoiceField(
+		queryset=Collection.objects.all(),
+		widget=Select2Multiple(select2attrs={"tags": "true"})
+	)
+
 	class Meta:
 		model = File
 		fields = ["file", "description", "source", "nsfw", "collections"]
-		widgets = {
-			"collections": Select2Multiple
-		}
